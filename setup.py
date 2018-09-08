a85b9ed5c11659a51d564fe8d2a2a14d3a90c4f2
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2018-09-08 20:13:23 +0900

    Add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+
+setup(
+    name='Mangaki Zero',
+    version='1.0',
+    description='Recommendation algorithms',
+    author='Jill-Jênn Vie',
+    author_email='vie@jill-jenn.net',
+    url='http://research.mangaki.fr',
+    python_requires='>=3.4',
+    install_requires=[
+        'numpy>=1.13.3,<=1.14.5',
+        'scipy>=1',
+        'scikit-learn',
+        'pandas',
+        'tensorflow',
+        'fastFM',
+        'pywFM',
+        'surprise'
+    ],
+    packages=['zero']
+)
