6f7767a37e989f28c09b55f3425837e177817b3b
Ryan Lahfa <masterancpp@gmail.com>
2020-04-29 05:31:20 +0200

    Use Poetry (#3)
    Add support for Poetry & basic Nix support.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 
 setup(
-    name='Mangaki Zero',
+    name='mangaki-zero',
     version='1.0',
     description='Recommendation algorithms',
     author='Jill-Jênn Vie',
     author_email='vie@jill-jenn.net',
-    url='http://research.mangaki.fr',
+    url='https://research.mangaki.fr',
     python_requires='>=3.4',
     install_requires=[
         'numpy>=1.13.3,<=1.18.2',
