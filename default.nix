c0c274501da8c7c2ef5c0c28499f712a28167002
Ryan Lahfa <masterancpp@gmail.com>
2020-05-03 02:13:54 +0200

    Add test matrix for Python 3.(6|7|8) & MKL/OpenBLAS

--- default.nix
+++ default.nix
@@ -1,4 +1,28 @@
-{ pkgs ? import <nixpkgs> {}, python ? pkgs.python3, useMKL ? true }:
+let ourNixpkgs =
+  builtins.fetchGit {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-unstable-2020-05-02";
+  url = "https://github.com/nixos/nixpkgs-channels/";
+  # Commit hash for nixos-unstable as of 2020-05-02
+  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
+  ref = "refs/heads/nixos-unstable";
+  rev = "fce7562cf46727fdaf801b232116bc9ce0512049";
+};
+in
+{ blasProvider ? "openblasCompat"
+, pkgs ? import ourNixpkgs {
+  config.allowUnfree = blasProvider == "mkl";
+  overlays = [
+    (self: super: {
+        lapack = super.lapack.override {
+          lapackProvider = super.${blasProvider};
+        };
+        blas = super.blas.override {
+          blasProvider = super.${blasProvider};
+        };
+      })]; }
+, pythonPackageName ? "python3"
+, python ? pkgs.${pythonPackageName}}:
 
 rec {
   pythonDependencies = (python.withPackages
